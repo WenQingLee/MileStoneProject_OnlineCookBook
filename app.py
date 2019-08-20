e3c666793a0f4b004fb604d60cc33418d0b9f55f
Ubuntu <ubuntu@ip-172-31-91-79.ec2.internal>
2019-08-20 14:12:54 +0000

    added the list of recipes for each type to the cards

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import os
 import pymongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -11,11 +12,15 @@ conn = pymongo.MongoClient(os.getenv("MONGO_URI"))
 @app.route("/")
 def index():
     
-    recipe_name = conn["online_cookbook"]["recipes"]["name"]
-
     recipes = conn["online_cookbook"]["recipes"].find({})
     
-    return render_template("index.html", recipes=recipes, recipe_name=recipe_name)
+    type_meat = conn["online_cookbook"]["recipes"].find({"type":"meat"})
+    
+    type_vegetable = conn["online_cookbook"]["recipes"].find({"type":"vegetable"})
+    
+    type_dessert = conn["online_cookbook"]["recipes"].find({"type":"dessert"})
+    
+    return render_template("index.html", recipes=recipes, type_meat=type_meat, type_vegetable=type_vegetable, type_dessert=type_dessert)
 
 
 
